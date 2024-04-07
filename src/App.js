365f10cb2db6008324332c4c50678972f23a542b
hasa-a-a <hassan_ali_ahmad@outlook.com>
2024-04-07 14:22:41 +0300

    add new posters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import "./App.css";
 
 const API_URL = 'https://www.omdbapi.com?apikey=4e66f495'
 
+const movie1 = {
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -27,9 +35,22 @@ const App = () => {
                 onChange={() =>{}}
                 />
                 <img src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
-      alt="search"
-      onClick={() => searchMovies(searchTerm)}/>
+                        alt="search"
+                        />
            </div>
+
+            <div className="container">
+                <div  className="movie" >
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== "N/A" ? movie1.Poster:"https://via.placeholder.com/400"}  alt={movie1.Title}/>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }
