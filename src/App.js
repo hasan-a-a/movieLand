eea0afd483f7f9c942d7a71a2ab6cb289ad3be67
hasa-a-a <hassan_ali_ahmad@outlook.com>
2024-04-07 14:25:06 +0300

    add new spam

--- src/App.js
+++ src/App.js
@@ -47,6 +47,9 @@ const App = () => {
                     <div>
                         <img src={movie1.Poster !== "N/A" ? movie1.Poster:"https://via.placeholder.com/400"}  alt={movie1.Title}/>
                     </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                    </div>
 
                 </div>
 
