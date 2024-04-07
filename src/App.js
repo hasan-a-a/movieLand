b893ab191873965529343a69c7a84f1db2eebf55
hasa-a-a <hassan_ali_ahmad@outlook.com>
2024-04-07 13:43:32 +0300

    add new featurs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
+import "./App.css";
 
 const API_URL = 'https://www.omdbapi.com?apikey=4e66f495'
 
@@ -17,7 +18,19 @@ const App = () => {
     },[])
 
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input type="text" placeholder="Search for movies"
+                value="superman"
+                onChange={() =>{}}
+                />
+                <img src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+      alt="search"
+      onClick={() => searchMovies(searchTerm)}/>
+           </div>
+        </div>
     )
 }
 export default App;
